43f989df3a56f8c1ffdad0303589c6ab2d89f0dc
ZiedYousfi <yousfi_zied@yahoo.com>
2026-01-20 13:12:07 +0100

    Add SDL3 subdirectory and initialize SDL in main function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <webgpu.h>
+#include <SDL3/SDL.h>
 
 int main(void) {
   printf("Hello wgpu-native !\n");
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+    return -1;
+  }
+
   return 0;
 }
