1394343d784e707a330a651b84cdaef149d5f849
ZiedYousfi <yousfi_zied@yahoo.com>
2026-01-20 14:27:21 +0100

    Improve SDL initialization and event handling in main function

--- src/main.c
+++ src/main.c
@@ -6,10 +6,32 @@
 int main(void) {
   printf("Hello wgpu-native !\n");
 
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
-    return -1;
+  // Init WebGPU
+  WGPUInstance instance = wgpuCreateInstance(NULL);
+
+  // Init SDL
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window = SDL_CreateWindow("Hello wgpu-native !", 640, 480, 0);
+
+  // Here we create our WebGPU surface from the window!
+  WGPUSurface surface = SDL_GetWGPUSurface(instance, window);
+
+  // Wait for close
+  SDL_Event event;
+  int running = 1;
+
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = 0;
+      }
+    }
+
+    SDL_Delay(16); // Simulate ~60fps
   }
+  // Terminate SDL
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
   return 0;
 }
